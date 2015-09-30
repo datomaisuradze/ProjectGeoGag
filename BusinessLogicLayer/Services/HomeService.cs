ea259face5e12cd0d16df2c095b821f13c9d6e54
David Maisuradze <datomaisuradze231995@gmail.com>
2015-09-30 17:25:46 +0400

    Updated

---
+++ BusinessLogicLayer/Services/HomeService.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLogicLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services
+{
+    public class HomeService
+    {
+        public IHomeRepository HomeRepository { get; set; }
+
+        public HomeService(IHomeRepository homeRepository)
+        {
+            this.HomeRepository = HomeRepository;
+        }
+    }
+}
